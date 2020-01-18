74bd1cd47db67af40b7a41b402f80e4e4a10122f
Dennis Moon <dmoon.comfycoder@gmail.com>
2020-01-18 15:36:23 -0600

    Removed references to core-js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,6 +61,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'core-js/es/object';
-import 'core-js/es/array';
-import 'core-js/es/reflect';
